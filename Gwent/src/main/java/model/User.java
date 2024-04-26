a5ca2ad65dcdc02aa0197d2875cf0f2e0c8ebce0
Bahar Barghbani <bahar.brqbni@gmail.com>
2024-04-26 17:40:27 +0330

    User is modified with wins, losses and draws, getters and setters

--- Gwent/src/main/java/model/User.java
+++ Gwent/src/main/java/model/User.java
@@ -12,6 +12,11 @@ public class User {
     private String nickName;
     private HashMap<Integer, String> securityQuestions;
     private ArrayList<GameHistory> gameHistories;
+    private int rank;
+    private int wins;
+    private int losses;
+    private int draws;
+
 
 
     public User(String username, String password, String email, String nickName) {
@@ -20,4 +25,30 @@ public class User {
         this.email = email;
         this.nickName = nickName;
     }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getNickName() {
+        return nickName;
+    }
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+
 }
