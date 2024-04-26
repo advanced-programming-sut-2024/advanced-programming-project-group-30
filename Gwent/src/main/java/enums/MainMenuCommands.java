abd231e4a73beab8e64731c67f0700d8328b795f
Bahar Barghbani <bahar.brqbni@gmail.com>
2024-04-26 17:29:47 +0330

    enums are modified with getMatcher

--- Gwent/src/main/java/enums/MainMenuCommands.java
+++ Gwent/src/main/java/enums/MainMenuCommands.java
@@ -1,4 +1,15 @@
 package enums;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum MainMenuCommands {
+    ;
+    private final String command;
+    MainMenuCommands(String command){
+        this.command = command;
+    }
+    public Matcher getMatcher(String input){
+        return Pattern.compile(this.command).matcher(input);
+    }
 }
--- Gwent/src/main/java/enums/StartGameMenuCommands.java
+++ Gwent/src/main/java/enums/StartGameMenuCommands.java
@@ -1,4 +1,15 @@
 package enums;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum StartGameMenuCommands {
+    ;
+    private final String command;
+    StartGameMenuCommands(String command){
+        this.command = command;
+    }
+    public Matcher getMatcher(String input){
+        return Pattern.compile(this.command).matcher(input);
+    }
 }
