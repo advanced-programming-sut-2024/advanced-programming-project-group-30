b4b89d68cac0ae1db219eeed281a2443c7d765c9
Fatemeh Shafiei <fatme.shafiei@gmail.com>
2024-05-02 01:43:49 +0330

    update App class

--- Gwent/src/main/java/model/App.java
+++ Gwent/src/main/java/model/App.java
@@ -10,33 +10,51 @@ public abstract class App {
     private static User loggedInUser;
     private static final ArrayList<User> allUsers = new ArrayList<>();
     private static final ArrayList<String> securityQuestions = new ArrayList<>();
-    static {
 
+    public static Menu getCurrentMenu() {
+        return currentMenu;
     }
 
+    public static void setCurrentMenu(Menu menu) {
+        currentMenu = menu;
+    }
 
+    public static User getLoggedInUser() {
+        return loggedInUser;
+    }
 
+    public static void setLoggedInUser(User loggedInUser) {
+        App.loggedInUser = loggedInUser;
+    }
 
+    public static Game getCurrentGame() {
+        return currentGame;
+    }
 
+    public static void setCurrentGame(Game currentGame) {
+        App.currentGame = currentGame;
+    }
 
-
-    public void addToAllUsers(User user){
+    public void addUser(User user) {
         allUsers.add(user);
     }
-    public User getUserByUsername(String username){
+
+    public void removeUser(User user) {
+        allUsers.remove(user);
+    }
+
+    public User getUserByUsername(String username) {
         for (User user : allUsers)
             if (user.getUsername().equals(username)) return user;
         return null;
 
     }
-    public static void setCurrentMenu(Menu menu){
-        currentMenu = menu;
-    }
-    public static Menu getCurrentMenu() {
-        return currentMenu;
-    }
-    public static int getUserRank(){
-        return 1;
+
+    public String getListOfSecurityQuestions() {
+        return null;
     }
 
+    public static int getUserRank() {
+        return 1;
+    }
 }
