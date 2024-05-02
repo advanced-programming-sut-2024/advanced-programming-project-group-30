4ac6eff0b3b7f33f5f20307deee32a4f835df7da
bahar <bahar.brqbni@gmail.com>
2024-05-02 17:53:02 +0330

    Leader Ability is added

---
+++ src/main/java/model/LeaderAbility.java
@@ -0,0 +1,145 @@
+package model;
+
+public abstract class LeaderAbility {
+    public abstract void run(Game currentGame);
+}
+class TheSiegemasterAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class TheSteelForgedAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class KingOfTemeriaAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class TheWhiteFlameAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class LordCommanderOfTheNorthAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class SonOfMedellAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class TheWhiteFalmeAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class HisImperialMajestyAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class EmporerOfNilfgaardAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class TheRelentlessAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class InvaderOfTheNorthAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class BringerOfDeathAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class KingOfTheWildHuntAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class DestroyerOfWorldsAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class CommanderOfTheRedRidersAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class TheTreacherousAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class QueenOfDolBlathannaAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class TheBeautifulAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class DaisyOfTheValleyAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class PureBloodElfAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class HopeOfTheAenSeidheAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class CrachAnCraiteAbility extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+class KingBran extends LeaderAbility {
+    @Override
+    public void run(Game currentGame) {
+
+    }
+}
+
+
--- src/main/java/model/Result.java
+++ src/main/java/model/Result.java
@@ -1,23 +1,24 @@
 package model;
 
+import enums.Error;
+
+import javax.swing.*;
+
 public class Result {
     private final String message;
     private final boolean isSuccessful;
-    private final boolean needFurtherInput;
+    private final Error error;
 
-    public Result(String message, boolean isSuccessful, boolean needFurtherInput) {
+    public Result(String message, boolean isSuccessful, Error error) {
         this.message = message;
         this.isSuccessful = isSuccessful;
-        this.needFurtherInput = needFurtherInput;
+        this.error = error;
     }
 
     public boolean isSuccessful() {
         return isSuccessful;
     }
 
-    public boolean isNeedFurtherInput() {
-        return needFurtherInput;
-    }
 
     @Override
     public String toString() {
