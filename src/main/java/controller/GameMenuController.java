1063a4895a7718d1e216d5baf6d28e20652fd626
bahar <bahar.brqbni@gmail.com>
2024-05-02 16:20:16 +0330

    Game menu controller is done

--- src/main/java/controller/GameMenuController.java
+++ src/main/java/controller/GameMenuController.java
@@ -1,9 +1,90 @@
 package controller;
 
+import model.Player;
 import model.Result;
 
 public class GameMenuController {
     public Result enterMenu(String menuName) {
+        return null;
+    }
+
+    public Result vetoCard(String cardName) {
+        return null;
+    }
+
+    public Result showHand() {
+        return null;
+    }
+
+    public Result showCardInHandInfo(String cardNumber) {
+        return null;
+    }
+
+    public Result showRemainingCardsInfo() {
+        return null;
+    }
+
+    public Result showOutOfPlayCards() {
+        return null;
+    }
+
+    private static String showPlayerOutOfPlayCards(Player player) {
+        return null;
+    }
+
+    public Result showCardsInRow() {
+        return null;
+    }
+
+    public Result showSpellCards() {
+        return null;
+    }
+
+    public Result placeCardInRow(String cardName, String rowNumber) {
+        return null;
+    }
+
+    public Result showCommanderInfo() {
+        return null;
+    }
+
+    public Result playCommanderPower() {
+        return null;
+    }
+
+    public Result showPlayersInfo() {
+        return null;
+    }
+
+    private static String showPlayerInfo(Player player) {
+        return null;
+    }
+
+    public Result showPlayerLives() {
+        return null;
+    }
+
+    public Result showNumberOfCardsInHand() {
+        return null;
+    }
+
+    public Result showTurnInfo() {
+        return null;
+    }
+
+    public Result showTotalScore() {
+        return null;
+    }
+
+    private static int showPlayerTotalScore(Player player) {
+        return 1;
+    }
+
+    public Result showTotalScoreOfRow(int rowNumber) {
+        return null;
+    }
+
+    public Result passRound() {
         return null;
     }
     public Result exitMenu() {
