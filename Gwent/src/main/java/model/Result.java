f8d8e259a9eb962521a2ad7ea0557de4de0cd520
Fatemeh Shafiei <fatme.shafiei@gmail.com>
2024-05-03 15:43:00 +0330

    update Result  from UML_bahar

--- Gwent/src/main/java/model/Result.java
+++ Gwent/src/main/java/model/Result.java
@@ -3,20 +3,20 @@ package model;
 public class Result {
     private final String message;
     private final boolean isSuccessful;
-    private final boolean needFurtherInput;
+    private final Error error;
 
-    public Result(String message, boolean isSuccessful, boolean needFurtherInput) {
+    public Result(String message, boolean isSuccessful, boolean needFurtherInput, Error error) {
         this.message = message;
         this.isSuccessful = isSuccessful;
-        this.needFurtherInput = needFurtherInput;
+        this.error = error;
     }
 
     public boolean isSuccessful() {
         return isSuccessful;
     }
 
-    public boolean isNeedFurtherInput() {
-        return needFurtherInput;
+    public Error getError() {
+        return error;
     }
 
     @Override
