c6d7f253be6f2651c167f1141dade4e24412420a
Fatemeh Shafiei <fatme.shafiei@gmail.com>
2024-05-03 20:07:40 +0330

    update SpecialCardsData enum

--- Gwent/src/main/java/enums/cardsData/SpecialCardsData.java
+++ Gwent/src/main/java/enums/cardsData/SpecialCardsData.java
@@ -1,5 +1,23 @@
 package enums.cardsData;
 
-public enum SpecialCardsData {
+import model.card.specialcard.SpecialCard;
 
+import java.util.ArrayList;
+
+public enum SpecialCardsData {
+    CARDS_NAME(0);
+
+    private final int cardsNumber;
+
+    SpecialCardsData(int cardsNumber) {
+        this.cardsNumber = cardsNumber;
+    }
+
+    private static SpecialCard createCard(SpecialCardsData specialCardsData) {
+        return null;
+    }
+
+    public static ArrayList<SpecialCard> getAllSpecialCard() {
+        return null;
+    }
 }
