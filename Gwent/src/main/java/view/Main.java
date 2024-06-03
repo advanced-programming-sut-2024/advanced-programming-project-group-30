ce2603a54c932e701379bd50e1efd4b8ed9b6b66
Bahar Barghbani <152790072+baharbarghbani@users.noreply.github.com>
2024-06-03 19:34:47 +0330

    Creating Main Menu fxml

--- Gwent/src/main/java/view/Main.java
+++ Gwent/src/main/java/view/Main.java
@@ -1,9 +1,20 @@
 package view;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Menu{
+import java.util.Objects;
 
+public class Main extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Scene scene = new Scene(fxmlLoader.load(Objects.requireNonNull(Main.class.getResource("/FXML/LoginMenu.fxml"))));
+        stage.setScene(scene);
+        stage.setTitle("Login Menu");
+        stage.show();
+    }
 
 }
--- Gwent/src/main/java/view/MainMenu.java
+++ Gwent/src/main/java/view/MainMenu.java
@@ -5,12 +5,18 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.util.Scanner;
 
 public class MainMenu extends Menu{
     private static MainMenuController mainMenuController;
+    @FXML
+    public Label username;
+    @FXML
+    public Label nickname;
+
 
     @FXML
     public void goToProfileMenu(){
