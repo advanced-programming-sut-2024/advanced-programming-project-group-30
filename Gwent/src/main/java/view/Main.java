5c7960f326c2fcf0e28fc55ed0874e94bfecc502
Fatemeh Shafiei <fatme.shafiei@gmail.com>
2024-06-03 16:33:14 +0330

    style username&password field and title and pane background in login menu

--- Gwent/src/main/java/view/Main.java
+++ Gwent/src/main/java/view/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader();
-        Scene scene = new Scene(fxmlLoader.load(Objects.requireNonNull(Main.class.getResourceAsStream("/FXML/LoginMenu.fxml"))));
+        Scene scene = new Scene(fxmlLoader.load(Objects.requireNonNull(Main.class.getResource("/FXML/LoginMenu.fxml"))));
         stage.setScene(scene);
         stage.setTitle("Login Menu");
         stage.show();
