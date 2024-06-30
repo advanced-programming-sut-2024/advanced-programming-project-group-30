d8722422cf0cfdeac2f77772d557ceef66eb0867
Fatemeh Shafiei <fatme.shafiei@gmail.com>
2024-06-30 13:43:18 +0330

    add feild to LgCard

--- Gwent/src/main/java/view/LgCard.java
+++ Gwent/src/main/java/view/LgCard.java
@@ -1,7 +1,24 @@
 package view;
 
+import enums.cardsData.CardData;
 import javafx.scene.shape.Rectangle;
 
 public class LgCard extends Rectangle {
+    private final int preGameHeight = 118;
+    private final int preGameWidth = 224;
+    private final int gameHeight = 0;
+    private final int gameWidth = 0;
+    private final CardData cardData;
+
+    public LgCard(CardData cardData) {
+        this.cardData = cardData;
+
+    }
+
+    public void setForPreGame(){
+        this.setHeight(preGameHeight);
+        this.setWidth(preGameWidth);
+    }
+
 
 }
