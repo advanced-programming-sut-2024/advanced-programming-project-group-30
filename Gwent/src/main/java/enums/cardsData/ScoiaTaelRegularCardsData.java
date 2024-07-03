a6f33e91d1073e7a7accfaf98eb609b31f713512
Fatemeh Shafiei <fatme.shafiei@gmail.com>
2024-07-03 15:54:09 +0330

    scoia get image in enum

--- Gwent/src/main/java/enums/cardsData/ScoiaTaelRegularCardsData.java
+++ Gwent/src/main/java/enums/cardsData/ScoiaTaelRegularCardsData.java
@@ -11,28 +11,43 @@ import java.util.Objects;
 
 public enum ScoiaTaelRegularCardsData implements RegularCardData {
     EITHNE("Eithne", null, true, 10, 1, RegularCardPositionType.RANGED_COMBAT),
-    ISENGRIM_FAOLITARNA("Isengrim Faolitarna", "MoralBoost", true, 10, 1, RegularCardPositionType.CLOSE_COMBAT),
-    LORVETH("Lorveth", null, true, 10, 1, RegularCardPositionType.RANGED_COMBAT),
-    MILVA("Milva", "MoralBoost", false, 10, 1, RegularCardPositionType.CLOSE_COMBAT),
-    SEASENTHESSIS("Seasenthessis", null, true, 10, 1, RegularCardPositionType.RANGED_COMBAT),
-    SCHIRRU("Schirru", "Scorch", false, 8, 1, RegularCardPositionType.SIEGE),
+    IORVETH("Iorveth", null, true, 10, 1, RegularCardPositionType.RANGED_COMBAT),
+    ISENGRIM_FAOLITARNA("Isengrim Faolitarna", "moralBoost", true, 10, 1, RegularCardPositionType.CLOSE_COMBAT),
+    MILVA("Milva", "moralBoost", false, 10, 1, RegularCardPositionType.CLOSE_COMBAT),
+    SAESENTHESSIS("Saesenthessis", null, true, 10, 1, RegularCardPositionType.RANGED_COMBAT),
+    SCHIRRU("Schirru", "scorch", false, 8, 1, RegularCardPositionType.SIEGE),
     BARCLAY_ELS("Barclay Els", null, false, 6, 1, RegularCardPositionType.AGILE),
     DENNIS_CRANMER("Dennis Cranmer", null, false, 6, 1, RegularCardPositionType.CLOSE_COMBAT),
     DOL_BLATHANNA_SCOUT("Dol Blathanna Scout", null, false, 6, 3, RegularCardPositionType.AGILE),
-    FILAVANDREL("Filavandrel", null, true, 6, 1, RegularCardPositionType.AGILE),
-    IDA_EMEAN_AEP("Ida Emean Aep", null, false, 6, 1, RegularCardPositionType.RANGED_COMBAT),
+    DOL_BLATHANNA_SCOUT_1("Dol Blathanna Scout", null, false, 6, 3, RegularCardPositionType.AGILE),
+    DOL_BLATHANNA_SCOUT_2("Dol Blathanna Scout", null, false, 6, 3, RegularCardPositionType.AGILE),
+    FILAVANDREL_AEN_FIDHAIL("Filavandrel aen Fidhail", null, true, 6, 1, RegularCardPositionType.AGILE),
+    IDA_EMEAN_AEP_SIVNEY("Ida Emean aep Sivney", null, false, 6, 1, RegularCardPositionType.RANGED_COMBAT),
     YAEVINN("Yaevinn", null, false, 6, 1, RegularCardPositionType.AGILE),
-    HAVEKAR_SMUGGLER("Havekar Smuggler", "Muster", false, 5, 3, RegularCardPositionType.CLOSE_COMBAT),
+    HAVEKAR_SMUGGLER("Havekar Smuggler", "muster", false, 5, 3, RegularCardPositionType.CLOSE_COMBAT),
+    HAVEKAR_SMUGGLER_1("Havekar Smuggler", "muster", false, 5, 3, RegularCardPositionType.CLOSE_COMBAT),
+    HAVEKAR_SMUGGLER_2("Havekar Smuggler", "muster", false, 5, 3, RegularCardPositionType.CLOSE_COMBAT),
     MAHAKAMAN_DEFENDER("Mahakaman Defender", null, false, 5, 5, RegularCardPositionType.CLOSE_COMBAT),
+    MAHAKAMAN_DEFENDER_1("Mahakaman Defender", null, false, 5, 5, RegularCardPositionType.CLOSE_COMBAT),
+    MAHAKAMAN_DEFENDER_2("Mahakaman Defender", null, false, 5, 5, RegularCardPositionType.CLOSE_COMBAT),
+    MAHAKAMAN_DEFENDER_3("Mahakaman Defender", null, false, 5, 5, RegularCardPositionType.CLOSE_COMBAT),
+    MAHAKAMAN_DEFENDER_4("Mahakaman Defender", null, false, 5, 5, RegularCardPositionType.CLOSE_COMBAT),
     VRIHEDD_BRIGADE_VETERAN("Vrihedd Brigade Veteran", null, false, 5, 2, RegularCardPositionType.AGILE),
+    VRIHEDD_BRIGADE_VETERAN_1("Vrihedd Brigade Veteran", null, false, 5, 2, RegularCardPositionType.AGILE),
     DOL_BLATHANNA_ARCHER("Dol Blathanna Archer", null, false, 4, 1, RegularCardPositionType.RANGED_COMBAT),
     VRIHEDD_BRIGADE_RECRUIT("Vrihedd Brigade Recruit", null, false, 4, 1, RegularCardPositionType.RANGED_COMBAT),
-    CIARAN_AEP("Ciaran Aep", null, false, 3, 1, RegularCardPositionType.AGILE),
-    DWARVEN_SKIRMISHER("Dwarven Skirmisher", "Muster", false, 3, 3, RegularCardPositionType.CLOSE_COMBAT),
-    ELVEN_SKIRMISHER("Elven Skirmisher", "Muster", false, 2, 3, RegularCardPositionType.RANGED_COMBAT),
+    CIARAN_AEP_EASNILLIEN("Ciaran aep Easnillien", null, false, 3, 1, RegularCardPositionType.AGILE),
+    DWARVEN_SKIRMISHER("Dwarven Skirmisher", "muster", false, 3, 3, RegularCardPositionType.CLOSE_COMBAT),
+    DWARVEN_SKIRMISHER_1("Dwarven Skirmisher", "muster", false, 3, 3, RegularCardPositionType.CLOSE_COMBAT),
+    DWARVEN_SKIRMISHER_2("Dwarven Skirmisher", "muster", false, 3, 3, RegularCardPositionType.CLOSE_COMBAT),
+    ELVEN_SKIRMISHER("Elven Skirmisher", "muster", false, 2, 3, RegularCardPositionType.RANGED_COMBAT),
+    ELVEN_SKIRMISHER_1("Elven Skirmisher", "muster", false, 2, 3, RegularCardPositionType.RANGED_COMBAT),
+    ELVEN_SKIRMISHER_2("Elven Skirmisher", "muster", false, 2, 3, RegularCardPositionType.RANGED_COMBAT),
     TORUVIEL("Toruviel", null, false, 2, 1, RegularCardPositionType.RANGED_COMBAT),
-    RIORDIAN("Riordian", null, false, 1, 1, RegularCardPositionType.RANGED_COMBAT),
-    HAVEKAR_HEALER("Havekar Healer", "Medic", false, 0, 3, RegularCardPositionType.RANGED_COMBAT),
+    RIORDAIN("Riordain", null, false, 1, 1, RegularCardPositionType.RANGED_COMBAT),
+    HAVEKAR_HEALER("Havekar Healer", "medic", false, 0, 3, RegularCardPositionType.RANGED_COMBAT),
+    HAVEKAR_HEALER_1("Havekar Healer", "medic", false, 0, 3, RegularCardPositionType.RANGED_COMBAT),
+    HAVEKAR_HEALER_2("Havekar Healer", "medic", false, 0, 3, RegularCardPositionType.RANGED_COMBAT),
     ;
 
     private final String name;
@@ -61,8 +76,8 @@ public enum ScoiaTaelRegularCardsData implements RegularCardData {
 
     @Override
     public Image getLgImage() {
-        String subAddress = this.name.toLowerCase().replaceAll(".*:", "").replace(" ", "_");
-        return new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/Images/Game/societal_" + subAddress)));
+        String address = "/Images/Game/LgCardsImages/scoiatael_" + this.toString().toLowerCase() + ".jpg";
+        return new Image(Objects.requireNonNull(this.getClass().getResourceAsStream(address)));
     }
 
     @Override
