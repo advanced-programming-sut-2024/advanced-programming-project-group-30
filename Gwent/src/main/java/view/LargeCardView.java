58a1554dbed7b7399d5dd346b9bff16ed882c7d4
Fatemeh Shafiei <fatme.shafiei@gmail.com>
2024-07-04 11:12:10 +0330

    modify largCardView

--- Gwent/src/main/java/view/LargeCardView.java
+++ Gwent/src/main/java/view/LargeCardView.java
@@ -1,26 +1,23 @@
 package view;
 
-import enums.FactionType;
-import enums.cardsData.CardData;
 import javafx.scene.image.Image;
 
 import java.io.InputStream;
 
 public class LargeCardView<T> extends Image {
-    private final T type;
+    private final T model;
     private final String descriptionTitle;
     private final String description;
 
     public LargeCardView(InputStream inputStream, T type, String descriptionTitle, String description) {
         super(inputStream);
-        if (!(type instanceof CardData) && !(type instanceof FactionType)) throw new RuntimeException("invalid type");
-        this.type = type;
+        this.model = type;
         this.descriptionTitle  = descriptionTitle;
         this.description = description;
     }
 
-    public T getType() {
-        return type;
+    public T getModel() {
+        return model;
     }
 
     public String getDescriptionTitle() {
