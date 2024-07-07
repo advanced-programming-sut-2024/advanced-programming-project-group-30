8b26fc6d4e2980f9c3e7369ae2538f1cc4d9c604
Fatemeh Shafiei <fatme.shafiei@gmail.com>
2024-07-07 13:35:52 +0330

    scale choose opponent menu

--- Gwent/src/main/java/view/ChooseOpponentMenu.java
+++ Gwent/src/main/java/view/ChooseOpponentMenu.java
@@ -1,27 +1,19 @@
 package view;
 
 import controller.ChooseOpponentController;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import model.App;
 import model.Result;
 
 public class ChooseOpponentMenu implements Menu {
     private final ChooseOpponentController controller = new ChooseOpponentController();
     @FXML
-    private Pane root;
-    @FXML
-    private VBox mainPane;
+    private TextField opponentUsername;
     @FXML
     private PasswordField opponentPassword;
-    @FXML
-    private TextField opponentUsername;
     @FXML
     private Label errorLabel;
 
